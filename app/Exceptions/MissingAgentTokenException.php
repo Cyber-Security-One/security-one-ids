f00cc482dd539ad421391af58d093403ab48c2f9
vito1317 <97001360+vito1317@users.noreply.github.com>
2026-03-11 09:12:44 +0000

    Fix: restore HTTP 503 status code for missing config exception

--- app/Exceptions/MissingAgentTokenException.php
+++ app/Exceptions/MissingAgentTokenException.php
@@ -8,6 +8,6 @@ class MissingAgentTokenException extends HttpException
 {
     public function __construct(string $message = 'The AGENT_TOKEN environment variable must be explicitly configured in production for API access.', \Throwable $previous = null, int $code = 0, array $headers = [])
     {
-        parent::__construct(401, $message, $previous, $headers, $code);
+        parent::__construct(503, $message, $previous, $headers, $code);
     }
 }
