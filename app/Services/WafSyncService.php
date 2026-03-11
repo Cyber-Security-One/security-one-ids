fa12b300a62c8b0280ab47f3471a93f6339a53ef
vito1317 <97001360+vito1317@users.noreply.github.com>
2026-03-11 11:31:37 +0000

    Fix newline handling in WafSyncService handleEnableLogin

--- app/Services/WafSyncService.php
+++ app/Services/WafSyncService.php
@@ -1646,7 +1646,7 @@ class WafSyncService
                     $user = trim($user);
                     if (!$user) continue;
                     
-                    $cleanUser = (string) preg_replace('/[\r\n]+/', ' ', $user);
+                    $cleanUser = (string) preg_replace('/[\r\n]+/', '', $user);
 
                     if (!preg_match('/^[a-zA-Z0-9._-]+$/', $cleanUser)) continue;
 
